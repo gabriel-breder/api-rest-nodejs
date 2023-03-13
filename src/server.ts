bd7231dc615c85c85daafe67ea44791204974339
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-03-13 18:15:03 -0300

    transactions routes and table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
-import fastify from 'fastify'
+import fastify from "fastify";
+import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
+import cookie from "@fastify/cookie";
 
-const app = fastify()
+const app = fastify();
+
+app.register(cookie);
+
+app.register(transactionsRoutes, {
+  prefix: "/transactions",
+});
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('HTTP Server Running!')
-  })
+    console.log("HTTP Server Running!");
+  });
